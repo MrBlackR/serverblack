cd80c2382b5cca7c7dcf8d511f8ffa5f9ce0ee58
MrBlackR <ctabilomery@gmail.com>
2024-07-13 21:23:58 +0000

    Mundo guardado 2024-07-13

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # Versiones disponibles
-# forge, mohist, fabric, vanilla, paper
+1# forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
 # Puedes instalar paper después de instalar vanilla desde el menú de gestionar
